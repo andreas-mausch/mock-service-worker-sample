bd850bd2b8726f2f5fc0635d57e40f1fd02613b5
neonew <neonew@gmail.com>
2023-02-19 19:46:11 +0100

    Revert "Add loading spinner"
    This reverts commit b11660b781b688671ce95840902c6d6f51d1c49d.

--- src/components/Artworks.vue
+++ src/components/Artworks.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
-import { PacmanLoader } from "vue3-spinner"
 
 interface ArtworkResponse {
   data: Artwork[]
@@ -14,25 +13,17 @@ interface Artwork {
 }
 
 const artworks = ref<Artwork[]>([])
-const loading = ref<boolean>(true)
 
 onMounted(async () => {
-  try {
-    const response = await axios.get<ArtworkResponse>('https://api.artic.edu/api/v1/artworks')
-    artworks.value = response.data.data
-  } finally {
-    loading.value = false
-  }
+  const response = await axios.get<ArtworkResponse>('https://api.artic.edu/api/v1/artworks')
+  artworks.value = response.data.data
 })
 </script>
 
 <template>
-  <PacmanLoader v-if="loading" />
-  <div v-else>
-    <p>I have found these artworks:</p>
+  <p>I have found these artworks:</p>
 
-    <div v-for="{ id, artist_title, title } in artworks" :key="id">
-      {{ artist_title }}: {{ title }}
-    </div>
+  <div v-for="{ id, artist_title, title } in artworks" :key="id">
+    {{ artist_title }}: {{ title }}
   </div>
 </template>
