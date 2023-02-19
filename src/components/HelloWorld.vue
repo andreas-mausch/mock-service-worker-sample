b11660b781b688671ce95840902c6d6f51d1c49d
neonew <neonew@gmail.com>
2023-02-19 08:34:32 +0100

    Add loading spinner

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
+import { PacmanLoader } from "vue3-spinner"
 
 interface ArtworkResponse {
   data: Artwork[]
@@ -13,17 +14,25 @@ interface Artwork {
 }
 
 const artworks = ref<Artwork[]>([])
+const loading = ref<boolean>(true)
 
 onMounted(async () => {
-  const response = await axios.get<ArtworkResponse>('https://api.artic.edu/api/v1/artworks')
-  artworks.value = response.data.data
+  try {
+    const response = await axios.get<ArtworkResponse>('https://api.artic.edu/api/v1/artworks')
+    artworks.value = response.data.data
+  } finally {
+    loading.value = false
+  }
 })
 </script>
 
 <template>
-  <p>I have found these artworks:</p>
+  <PacmanLoader v-if="loading" />
+  <div v-else>
+    <p>I have found these artworks:</p>
 
-  <div v-for="{ id, artist_title, title } in artworks" :key="id">
-    {{ artist_title }}: {{ title }}
+    <div v-for="{ id, artist_title, title } in artworks" :key="id">
+      {{ artist_title }}: {{ title }}
+    </div>
   </div>
 </template>
