d84c81ca124d5353829f5989492cef112bc3a722
neonew <neonew@gmail.com>
2023-02-20 08:15:01 +0100

    Add spinner again, but just as an svg

--- src/components/Artworks.vue
+++ src/components/Artworks.vue
@@ -13,17 +13,31 @@ interface Artwork {
 }
 
 const artworks = ref<Artwork[]>([])
+const loading = ref<boolean>(true)
 
 onMounted(async () => {
-  const response = await axios.get<ArtworkResponse>('https://api.artic.edu/api/v1/artworks')
-  artworks.value = response.data.data
+  try {
+    const response = await axios.get<ArtworkResponse>('https://api.artic.edu/api/v1/artworks')
+    artworks.value = response.data.data
+  } finally {
+    loading.value = false
+  }
 })
 </script>
 
 <template>
-  <p>I have found these artworks:</p>
+  <img src="/pacman.svg" class="spinner" v-if="loading" />
+  <div v-else>
+    <p>I have found these artworks:</p>
 
-  <div v-for="{ id, artist_title, title } in artworks" :key="id">
-    {{ artist_title }}: {{ title }}
+    <div v-for="{ id, artist_title, title } in artworks" :key="id">
+      {{ artist_title }}: {{ title }}
+    </div>
   </div>
 </template>
+
+<style scoped>
+.spinner {
+  height: 80px;
+}
+</style>
