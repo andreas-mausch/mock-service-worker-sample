84428fcf82cc4257e1be6554295dd815ce1f3272
neonew <neonew@gmail.com>
2023-03-01 18:49:25 +0100

    Setup global fetch for vitest tests
    See: https://markus.oberlehner.net/blog/using-mock-service-worker-with-vitest-and-fetch/

--- test/setup.ts
+++ test/setup.ts
@@ -1,11 +1,12 @@
 import { setupServer } from "msw/node"
 import { afterAll, afterEach, beforeAll } from "vitest"
 import { handlers } from "@/mocks/handlers"
+import { fetch } from "cross-fetch"
+
+global.fetch = fetch
 
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }))
-
 afterAll(() => server.close())
-
 afterEach(() => server.resetHandlers())
